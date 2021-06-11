1375366a25a63f5185426913fc20942a6a0904da
ZayenJS <davidnogueira.dev.pro@gmail.com>
2021-06-11 16:58:46 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Accueil";
+$css = "home";
+require_once './includes/header.php';
+?>
+
+<div class="container">
+	<h1 class="brand--text">Ristorante</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet blanditiis voluptas velit at ea. Harum
+		eveniet laudantium ducimus itaque dolore ratione non voluptates dolor, saepe provident vel deleniti assumenda
+		reprehenderit?</p>
+	<a class="contact-us" href="contact.php">Contactez nous</a>
+</div>
+
+<?php require_once './includes/footer.php';?>
---
+++ menu.php
@@ -0,0 +1,47 @@
+<?php
+$title = "Menu";
+$css = "menu";
+require_once './includes/header.php';?>
+
+<div class="container">
+	<ul>
+		<li>
+			<h2>Entrées</h2>
+			<ul>
+				<li><a href="#">AnitiPasti</a></li>
+				<li><a href="#">Salade</a></li>
+				<li><a href="#">Carpaccio</a></li>
+				<li><a href="#">Raviole</a></li>
+			</ul>
+		</li>
+		<li>
+			<h2>Plats</h2>
+			<ul>
+				<li><a href="#">Lasagnes</a></li>
+				<li><a href="#">Pizza</a></li>
+				<li><a href="#">Gratin</a></li>
+				<li><a href="#">Pasta</a></li>
+			</ul>
+		</li>
+		<li>
+			<h2>Desserts</h2>
+			<ul>
+				<li><a href="#">Cannoli</a></li>
+				<li><a href="#">Tiramisu</a></li>
+				<li><a href="#">Cheescake</a></li>
+				<li><a href="#">Panna Cotta</a></li>
+			</ul>
+		</li>
+		<li>
+			<h2>Boissons</h2>
+			<ul>
+				<li><a href="#">S.Pellegrino</a></li>
+				<li><a href="#">Cocktail</a></li>
+				<li><a href="#">Vin rouge</a></li>
+				<li><a href="#">Vin blanc</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+<?php require_once './includes/footer.php';?>
